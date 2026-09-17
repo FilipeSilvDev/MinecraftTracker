9fa57861ba39e52066e71dc78bd66d116ef9132d
Filipe Silveira <contato@filipesilveira.dev>
2026-09-16 21:18:46 -0300

    -- release: 1.0.0

--- obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/MinecraftTracker.AssemblyInfo.cs
+++ obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/MinecraftTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinecraftTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b50546add41da01f212e70110ebab1f63e57a0cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinecraftTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinecraftTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
